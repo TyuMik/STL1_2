0a5a3152e4ded5bbcc41609ff64a05e0e7f54422
Mikhail <miktyu88@gmail.com>
2024-05-04 17:04:53 +0300

    Initial commit

---
+++ STL1_2/STL1_2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <set>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    std::cout << "Введите число элементов: ";
+    int num, elem;
+    std::cin >> num;
+
+    std::set<int> m;
+
+    for (int i = 0; i < num; i++) {
+        std::cout << "Введите " << i + 1 << "-й элемент: ";
+        std::cin >> elem;
+        m.insert(elem);
+    }
+
+    std::cout << std::endl;
+
+    std::set<int>::reverse_iterator itr;
+
+    for (auto itr = m.rbegin(); itr != m.rend(); itr++) {
+        std::cout << *itr << std::endl;
+    }
+
+    return 0;
+}
